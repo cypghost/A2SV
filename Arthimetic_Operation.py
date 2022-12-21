015047109cf4ec3fae931b1f365e9ef969785805
BIRUK M <102286732+cypghost@users.noreply.github.com>
2022-12-21 17:50:29 +0300

    Update Arthimetic_Operation.py

--- Arthimetic_Operation.py
+++ Arthimetic_Operation.py
@@ -1,9 +1,6 @@
 if __name__ == '__main__':
     a = int(input())
-    b = int(input())
-    add = a+b
-    sub = a-b
-    mul = a*b
-    print(add)
-    print(sub)
-    print(mul)
+    b = int(input()) 
+    print(a+b)
+    print(a-b)
+    print(a*b)
