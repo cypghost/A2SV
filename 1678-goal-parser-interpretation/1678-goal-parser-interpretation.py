fee6cab6a982e0479b4273ffe22fc9fb20e24ff9
BIRUK M <102286732+cypghost@users.noreply.github.com>
2022-12-24 12:01:09 +0300

    Time: 36 ms (80.88%), Space: 13.9 MB (56.60%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -5,16 +5,12 @@ class Solution:
         answer = ""
         
         # Check if empty or not and to find strings and "()" to replace with o
-        if command == "":
-            return 0
-        
-        else:
-            for i in range(len(command)):
-                if command[i] != "(" and command[i] != ")":
-                    answer += command[i]
+        for i in range(len(command)):
+            if command[i] != "(" and command[i] != ")":
+                answer += command[i]
                     
-                elif  command[i] == "(" and command[i+1] == ")":
-                    answer += "o"
+            elif  command[i] == "(" and command[i+1] == ")":
+                answer += "o"
         return answer          
         
             
