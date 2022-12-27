3655d16fd31f742df659c79431f4074784662631
BIRUK M <102286732+cypghost@users.noreply.github.com>
2022-12-27 19:54:35 +0300

    Time: 619 ms (18.48%), Space: 15.6 MB (16.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -5,6 +5,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+         
         temp = nums.count(0)
         
         for i in range(temp):
@@ -12,21 +13,4 @@ class Solution:
             nums.append(0)
             
         return nums
-        
-    
-    
-    
-    
-    
-    
-    
-        q = deque(nums)
-        
-        for i in range(len(nums)):
-            if nums[i]== 0:
-                q.append(nums[i])
-            else:
-                q.appendleft(nums[i])
-                
-        return q
             
