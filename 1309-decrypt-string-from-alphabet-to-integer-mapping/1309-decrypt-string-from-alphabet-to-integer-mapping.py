1fbc63bdbd19ef70df6ed65d2f2cf2a4074550fe
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-04 16:07:25 +0300

    Update 1309-decrypt-string-from-alphabet-to-integer-mapping.py

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -19,3 +19,7 @@ class Solution(object):
                 index += 1 
                 
         return ''.join(ans)
+    
+ 
+#         for i in range(26,0,-1): s = s.replace(str(i)+'#'*(i>9),chr(96+i))
+#         return s
