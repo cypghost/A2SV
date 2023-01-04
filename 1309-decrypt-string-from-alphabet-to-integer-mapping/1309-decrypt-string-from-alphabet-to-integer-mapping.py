7708897ed27314079797fa170bb5c5d35da82b4d
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-04 16:04:42 +0300

    Time: 29 ms (39.39%), Space: 13.6 MB (20.61%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def freqAlphabets(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ans = []
+        index = 0
+        
+        while index < len(s):
+            
+            if index + 2 < len(s) and s[index + 2] == '#':
+                character = int(s[index : index + 2])
+                ans.append(chr(character + 96))
+                index += 3 
+                
+            else:
+                ans.append(chr(int(s[index]) + 96))
+                index += 1 
+                
+        return ''.join(ans)
