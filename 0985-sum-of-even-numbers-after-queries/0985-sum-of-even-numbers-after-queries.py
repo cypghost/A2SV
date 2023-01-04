f4be3cd5b3d5c660ea29ae982adbc17f6a53f23f
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-04 19:14:14 +0300

    Time: 917 ms (45.45%), Space: 19.8 MB (14.55%) - LeetHub

---
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.py
@@ -0,0 +1,81 @@
+class Solution(object):
+    def sumEvenAfterQueries(self, nums, queries):
+        """
+        :type nums: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        answer = []
+        evensum = 0
+        
+        for index in range(len(nums)):
+            if nums[index] % 2 == 0:
+                evensum += nums[index]
+        
+        for queryrow in range(len(queries)):
+            index = queries[queryrow][1]
+            value = queries[queryrow][0]
+            
+            numsele = nums[index]
+            
+            nums[index] += value
+            
+            if nums[index] % 2 == 0 and numsele % 2 != 0:
+                evensum += nums[index]
+                
+            if nums[index] % 2 == 0 and numsele % 2 == 0:
+                evensum += value
+                
+            elif nums[index] % 2 != 0 and numsele % 2 == 0:
+                evensum -= numsele
+            
+            answer.append(evensum)
+            
+        return answer
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         for queryrow in range(len(queries)):
+            
+#             index = queries[queryrow][1]
+#             value = queries[queryrow][0]
+#             evensum = 0
+            
+#             # to add value to the nums[index] value
+#             nums[index] += value
+            
+#             # To find even numbers and sum them up
+#             for index in range(len(nums)):
+#                  if nums[index] % 2 == 0:
+#                     evensum += nums[index]
+                
+#             answer.append(evensum) 
+            
+#         return answer
+                
+            
+            
