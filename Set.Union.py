bc6c67ae628f60072e4317a33a666c2c4e7dbfb2
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-06 12:35:01 +0300

    Create Set.Union.py

---
+++ Set.Union.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+A = int(input())
+setA = set(map(int, input().split()))
+B = int(input())
+setB = set(map(int, input().split()))
+
+print(len(setA.union(setB)))
