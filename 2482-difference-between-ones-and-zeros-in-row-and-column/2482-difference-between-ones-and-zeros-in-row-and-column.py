bc68d81e01c6d098e20530c3d5e0eecdbc99297b
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-09 00:13:20 +0300

    Time: 1457 ms (90.12%), Space: 56.6 MB (37.79%) - LeetHub

---
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def onesMinusZeros(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        rows = [0] * len(grid)
+        cols = [0] * len(grid[0])
+    
+        for gridrow in range(len(grid)):
+            for gridcol in range(len(grid[0])):
+                
+                # value will be -1 if the element is 0 otherwise 1
+                value = 2 * grid[gridrow][gridcol] - 1 
+                
+                # operation of onesrow and zerosrow will be made
+                rows[gridrow] += value
+                cols[gridcol] += value
+        
+        # return the difference with in the specified row and column 
+        return [[rows[gridrow] + cols[gridcol] 
+                for gridcol in range(len(grid[0]))] 
+                for gridrow in range(len(grid))]
