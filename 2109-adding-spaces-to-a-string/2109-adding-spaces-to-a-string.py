fdd93f0b4dc70ba228ee08313caa03f1f96d0c01
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-09 01:13:25 +0300

    Time: 484 ms (100.00%), Space: 48.6 MB (95.24%) - LeetHub

---
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def addSpaces(self, s, spaces):
+        """
+        :type s: str
+        :type spaces: List[int]
+        :rtype: str
+        """
+        answer = []
+        number = 0
+        
+		# stores a word to the list separately from the string
+        for index in spaces:
+            word = s[number:index]
+            answer.append(word)
+            number = index
+            
+        # left out string characters are added 
+        answer.append(s[spaces[len(spaces) - 1]:])
+        
+        return " ".join(answer)
+        
+        # TIME LIMIT EXCEEDED        
+#         number = 0
+#         add = 0
+        
+#         while number < len(spaces):
+            
+#             s = s[:spaces[number] + add] + " " +s[spaces[number] + add:]
+#             number += 1
+#             add += 1
+        
+#         return s
+			
