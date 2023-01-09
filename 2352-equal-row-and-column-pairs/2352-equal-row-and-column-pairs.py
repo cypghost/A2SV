85eefd1c366a83c72754d80bde128e222646efa2
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-09 23:18:13 +0300

    Time: 2114 ms (6.94%), Space: 17.8 MB (79.17%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def equalPairs(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        number = len(grid)
+        answer = 0
+        
+        for row in range(number):
+            for col in range(number):
+                found = True
+                
+                for index in range(number):
+                    if grid[row][index] != grid[index][col]:
+                        found = False
+                        break
+                    
+                if found:
+                    answer += 1
+					
+        return answer
