40dd03f2b8514267bdf46c57ee8edcd5273afaed
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-09 18:16:33 +0300

    Time: 42 ms (85.02%), Space: 13.9 MB (28.02%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.py
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -6,39 +6,6 @@ class Solution(object):
         :type cpdomains: List[str]
         :rtype: List[str]
         """
-#         domainssub = defaultdict()
-        
-#         for domain in cpdomains:
-#             rep, domain = domain.split()
-            
-#             rep = int(rep)
-#             subdomain = domain.split('.')
-            
-#             for char in range(len(subdomain)):
-#                 domainssub[".".join(subdomain[char:])] += rep
-                
-#         return ["{rep} {domain}" for domain, rep in domainssub.items()]
-    
-#         domainssub = defaultdict()
-        
-#         for domain in cpdomains:
-            
-#             domain = domain.split(' ')
-#             subdomain = domain[1].split('.')
-            
-#             char = 0
-#             while char != -1:
-#                 char = domain[1].find('.', char + 1)
-#                 domainssub[domain[1][char + 1:]] += int(domain[0])
-                
-#         answer = []
-        
-#         for rep, domain in domainssub.items():
-#             answer.append(str(rep) + " " + domain)
-            
-#         return answer
-
-
         domainssub = defaultdict(lambda:0)
     
         for domain in cpdomains:
@@ -46,7 +13,7 @@ class Solution(object):
             domains = subdomain[1].split('.')
             char = 0
             
-            while char != -1:
+            while char >= 0:
                 char = subdomain[1].find('.', char + 1)
                 domainssub[subdomain[1][char + 1:]] += int(subdomain[0])
                 
