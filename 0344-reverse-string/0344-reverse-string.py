75b407cfc990407c259ba5bb3f491373703e00ad
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-11 19:25:13 +0300

    Time: 140 ms (99.40%), Space: 21.2 MB (27.81%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        # for index in range(len(s) - 1, len(s)//2 , -1):
-        #     # temp = s[index]
-        #     # s[index] = s[-1-index]
-        #     # s[-1-index] = temp
+        for index in range(len(s) - 1, len(s)//2 - 1 , -1):
+            temp = s[index]
+            s[index] = s[-1-index]
+            s[-1-index] = temp
         
-         # for index in range(len(s) - 1, len(s)//2 , -1):
-         #    s[index], s[-1-index] = s[-1-index], s[index]
+        # for index in range(len(s) - 1, len(s)//2 - 1 , -1):
+        #     s[index], s[-1-index] = s[-1-index], s[index]
             
           
-        return s.reverse()
+        # return s.reverse()
         
