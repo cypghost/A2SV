cbdfa0252a75b97a892ae5d598dc1aae7ff0d5bb
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-11 19:30:55 +0300

    Update 0344-reverse-string.py

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,14 +4,16 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        # for index in range(len(s) - 1, len(s)//2 - 1 , -1):
-        #     temp = s[index]
-        #     s[index] = s[-1-index]
-        #     s[-1-index] = temp
-        
-        for index in range(len(s) - 1, len(s)//2 - 1 , -1):
-            s[index], s[-1-index] = s[-1-index], s[index]
+#         Solution 3: 65% runtime 95% space 
+#         for index in range(len(s) - 1, len(s)//2 - 1 , -1):
+#             temp = s[index]
+#             s[index] = s[-1-index]
+#             s[-1-index] = temp
+
+#         Solution 2: 99% runtime 
+#         for index in range(len(s) - 1, len(s)//2 - 1 , -1):
+#             s[index], s[-1-index] = s[-1-index], s[index]
             
-          
-        # return s.reverse()
+#         Solution 1: 96% runtime  
+          return s.reverse()
         
