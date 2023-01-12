451630f99aab1d0f26cd57a66a1376757b1c5ed4
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-12 18:23:03 +0300

    Time: 162 ms (80.66%), Space: 14.4 MB (96.26%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -5,28 +5,9 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        # Problem 1
-#         pointer = 1
-#         index = 0
         
-#         while index < len(strs[0]) and pointer <= len(strs) - 1:
-#             print(strs[pointer][index], strs[pointer - 1][index])
-#             if strs[pointer][index] < strs[pointer - 1][index]:
-#                     count += 1
-#                     pointer += 1
-#             index += 1
-        
-        # Problem 2 
-#         pointer = 0
-#         index = 0
-        
-#         while index < len(strs[0]) and pointer < len(strs) - 1:
-#             if strs[pointer][index] > strs[pointer + 1][index]:
-#                     count += 1
-#                     pointer += 1
-#             index += 1
-
         for char in range(len(strs[0])):
+            
             for index in range(len(strs) - 1):
                 if ord(strs[index][char]) > ord(strs[index + 1][char]):
                     count += 1
@@ -34,30 +15,5 @@ class Solution(object):
         
         return count
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#         for char in range(len(strs[0])):
-#             for index in range(len(strs)):
-#                 if ord(strs[index][char]) < ord(strs[index - 1][char]):
-#                     count += 1
-#                     break
-                
 
     
