bb58263e52572966969bff80c3e903a6712f3842
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-12 18:21:54 +0300

    Time: 173 ms (77.78%), Space: 14.7 MB (37.98%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -6,11 +6,11 @@ class Solution(object):
         """
         count = 0
         # Problem 1
-        
 #         pointer = 1
 #         index = 0
         
 #         while index < len(strs[0]) and pointer <= len(strs) - 1:
+#             print(strs[pointer][index], strs[pointer - 1][index])
 #             if strs[pointer][index] < strs[pointer - 1][index]:
 #                     count += 1
 #                     pointer += 1
@@ -20,17 +20,44 @@ class Solution(object):
 #         pointer = 0
 #         index = 0
         
-#         while index < len(strs[0]) and pointer <= len(strs) - 1:
+#         while index < len(strs[0]) and pointer < len(strs) - 1:
 #             if strs[pointer][index] > strs[pointer + 1][index]:
 #                     count += 1
 #                     pointer += 1
 #             index += 1
 
         for char in range(len(strs[0])):
-            for index in range(1, len(strs)):
-                if ord(strs[index][char]) < ord(strs[index - 1][char]):
+            for index in range(len(strs) - 1):
+                if ord(strs[index][char]) > ord(strs[index + 1][char]):
                     count += 1
                     break
-                
+        
         return count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         for char in range(len(strs[0])):
+#             for index in range(len(strs)):
+#                 if ord(strs[index][char]) < ord(strs[index - 1][char]):
+#                     count += 1
+#                     break
+                
+
     
