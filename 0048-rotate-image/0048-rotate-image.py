1ba8a2d761f78887b501637ce5472ecec80c05b0
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-16 18:27:26 +0300

    Time: 11 ms (99.83%), Space: 13.3 MB (93.38%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        answer = []
+        
+        for row in range(len(matrix)):
+            shifted = []
+            for col in range(len(matrix[0])):
+                shifted.append(matrix[col][row])
+            answer.append(shifted[::-1])
+            
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                matrix[row][col] = answer[row][col]
+            
+            
