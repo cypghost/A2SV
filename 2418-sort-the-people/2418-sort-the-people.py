7263a722d1fbf2d15b8ad44f9ab94b3e2b211431
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-17 19:01:26 +0300

    Time: 1135 ms (6.99%), Space: 14.1 MB (28.24%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -5,21 +5,21 @@ class Solution(object):
         :type heights: List[int]
         :rtype: List[str]
         """
-        # for index in range(len(names)):
-        #     for elements in range(index, len(heights) - 1):
-        #         if heights[elements] < heights[elements + 1]:
-        #             heights[elements], heights[elements + 1] = heights[elements + 1], heights[elements]
-        #             names[elements], names[elements + 1] = names[elements + 1], names[elements]
+        for index in range(len(names)):
+            for elements in range(len(heights) - 1 - index):
+                if heights[elements] < heights[elements + 1]:
+                    heights[elements], heights[elements + 1] = heights[elements + 1], heights[elements]
+                    names[elements], names[elements + 1] = names[elements + 1], names[elements]
         
         
-        for index in range(len(heights)):
-            minindex = index
+#         for index in range(len(heights)):
+#             minindex = index
             
-            for element in range(index , len(heights)):
-                if heights[element] > heights[minindex]:
-                    minindex = element
+#             for element in range(index , len(heights)):
+#                 if heights[element] > heights[minindex]:
+#                     minindex = element
                     
-            heights[minindex], heights[index] = heights[index], heights[minindex]
-            names[minindex], names[index] = names[index], names[minindex]
+#             heights[minindex], heights[index] = heights[index], heights[minindex]
+#             names[minindex], names[index] = names[index], names[minindex]
                     
         return names
