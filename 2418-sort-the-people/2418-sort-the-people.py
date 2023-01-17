2adb942bff7b5273a990556bf989832c3b397a16
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-17 18:50:24 +0300

    Time: 437 ms (13.21%), Space: 14 MB (55.96%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
+        # for index in range(len(names)):
+        #     for elements in range(index, len(heights) - 1):
+        #         if heights[elements] < heights[elements + 1]:
+        #             heights[elements], heights[elements + 1] = heights[elements + 1], heights[elements]
+        #             names[elements], names[elements + 1] = names[elements + 1], names[elements]
+        
+        
+        for index in range(len(heights)):
+            minindex = index
+            
+            for element in range(index , len(heights)):
+                if heights[element] > heights[minindex]:
+                    minindex = element
+                    
+            heights[minindex], heights[index] = heights[index], heights[minindex]
+            names[minindex], names[index] = names[index], names[minindex]
+                    
+        return names
