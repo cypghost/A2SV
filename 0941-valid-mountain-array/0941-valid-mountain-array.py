0b9cad0cc968bee8c0f3eb7a9fd61c463f5c7453
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-20 14:14:12 +0300

    Update 0941-valid-mountain-array.py

--- 0941-valid-mountain-array/0941-valid-mountain-array.py
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -1,12 +1,5 @@
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        '''
-        
-        The first and last elements of the array need to be the lowest compared to those in the middle
-        
-        
-        
-        '''
         # 1. check if arr length < 3
         if len(arr) < 3:
             return False
@@ -24,13 +17,15 @@ class Solution:
             if arr[index] > peak:
                 peak = arr[index]
                 value = index
-                
+         
+        # 4. check if peak is located at the first or the last of the array
         if value == 0:
             return False
     
         if value == len(arr) - 1:
             return False
         
+        # 5. check to see if it satisfies the second condition 
         for index in range(value - 1):
             if arr[index] > arr[index + 1]:
                 return False
