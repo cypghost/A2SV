04049b7951e7554aaaeeb6756afbe41bd3320370
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-22 19:49:19 +0300

    Time: 80 ms (28.06%), Space: 14 MB (5.00%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,20 +1,20 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        numstr = sorted([str(num) for num in nums])
-        curr = [numstr[0]]
-        
+        string = sorted([str(num) for num in nums])
+        curr = [string[0]]
+
         for index in range(1, len(nums)):
-            if curr[-1] + numstr[index] > numstr[index] + curr[-1]:
-                numstr[index - len(curr)] = numstr[index]
-                numstr[index] = curr[-1]
+            if curr[-1] + string[index] > string[index] + curr[-1]:
+                string[index - len(curr)] = string[index]
+                string[index] = curr[-1]
                 
-            elif curr[-1] + numstr[index] == numstr[index] + curr[-1]:
-                curr.append(numstr[index])
+            elif curr[-1] + string[index] == string[index] + curr[-1]:
+                curr.append(string[index])
                 
             else:
-                curr = [numstr[index]]
+                curr = [string[index]]
         
-        result = ''.join(reversed(numstr)) 
+        result = ''.join(reversed(string)) 
         
         if int(result) > 0:
             return result
