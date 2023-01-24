5ba1e639433d3c16962edae53fa020ef171fdfb3
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-24 22:28:51 +0300

    Time: 162 ms (94.59%), Space: 15.6 MB (58.02%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,19 +3,35 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        element = 0
+        left = 0
+        right = 1
         
-        for index in range(len(nums)):
-             if nums[index] != 0:
-                temp = nums[index]
-                nums[index] = nums[element]
-                nums[element] = temp
-                element += 1
+        while right <= len(nums) - 1:
+            if nums[left] != 0:
+                left += 1
                 
-                # nums[index], nums[element] = nums[element], nums[index]
+            elif nums[right] != 0 and nums[left] == 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+       
+            right += 1
+            
+        return nums  
+    
+    
+#         element = 0
+        
+#         for index in range(len(nums)):
+#              if nums[index] != 0:
+#                 temp = nums[index]
+#                 nums[index] = nums[element]
+#                 nums[element] = temp
+#                 element += 1
+                
+#                 # nums[index], nums[element] = nums[element], nums[index]
                 
                 
-        return nums
+        
         
         
         
