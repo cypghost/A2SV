e2e6d446795f4f4b467ec5c04fd9745a90642c5d
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-24 16:20:18 +0300

    Time: 43 ms (79.95%), Space: 13.9 MB (26.98%) - LeetHub

---
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -0,0 +1,24 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        
+        size = len(arr)
+        ans = []
+        
+        while size > 1:
+            
+            large = arr.index(size)
+            
+            if large == size - 1:
+                size -= 1
+                continue
+
+            if large != 0:
+                arr[:large + 1] = reversed(arr[:large + 1])
+                ans.append(large + 1) 
+                
+            arr[:size] = reversed(arr[:size])
+            ans.append(size)
+            
+            size -= 1 
+            
+        return ans  
