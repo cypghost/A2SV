c28e8046376b9039256044fe342cce6927f3464a
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-01-24 16:24:54 +0300

    Time: 54 ms (41.09%), Space: 13.8 MB (97.28%) - LeetHub

--- 0969-pancake-sorting/0969-pancake-sorting.py
+++ 0969-pancake-sorting/0969-pancake-sorting.py
@@ -7,18 +7,18 @@ class Solution:
         while size > 1:
             
             large = arr.index(size)
-            
+                
             if large == size - 1:
                 size -= 1
                 continue
 
-            if large != 0:
+            elif large != 0:
                 arr[:large + 1] = reversed(arr[:large + 1])
                 ans.append(large + 1) 
-                
+            
             arr[:size] = reversed(arr[:size])
             ans.append(size)
-            
+                
             size -= 1 
             
         return ans  
