bb7eb7a96f4e16cee1dd7dadf995fda64904b066
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-02-19 16:00:34 +0300

    Time: 33 ms (66.59%), Space: 13.9 MB (13.37%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,60 +5,56 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Approach 2
-        
-        if not head or not head.next:
-            return head
-        
-        temp = ListNode()
-        temp.next = head
-        
-        header = temp
-        
-        curr = head
-        
-        list1 = head
-        list2 = head.next
-        
-        while list1 and list2:
-            curr = list2.next
-            header.next = list2
-            
-            list2.next = list1
-            list1.next =None
-            
-            header = list1
-            list1 = curr
-            
-            if curr:
-                list2 = curr.next
-                
-        if curr:
-            header.next = curr
-            
-        return temp.next
-            
-        
         # Approach 1
         
-#         if head == None or head.next == None:
+        if head == None or head.next == None:
+            return head
+        
+        fast = head
+        slow = head.next
+        
+        while slow and fast:
+            slow.val, fast.val = fast.val, slow.val
+            
+            if fast.next:
+                fast = fast.next.next
+            
+            if slow.next:
+                slow = slow.next.next
+        
+        return head
+
+
+        # Approach 2
+        
+#         if not head or not head.next:
 #             return head
         
-#         fast = head
-#         slow = head.next
+#         temp = ListNode()
+#         temp.next = head
         
+#         header = temp
         
-#         while slow.next and fast:
-#             value = fast.val
-            
-#             fast.val = slow.val
-#             slow.val = value
-            
-#             fast = fast.next.next
-#             slow = slow.next.next
-            
-#         value = fast.val
-#         fast.val = slow.val 
-#         slow.val = value
+#         curr = head
         
-#         return head
+#         list1 = head
+#         list2 = head.next
+        
+#         while list1 and list2:
+#             curr = list2.next
+#             header.next = list2
+            
+#             list2.next = list1
+#             list1.next =None
+            
+#             header = list1
+#             list1 = curr
+            
+#             if curr:
+#                 list2 = curr.next
+                
+#         if curr:
+#             header.next = curr
+            
+#         return temp.next
+            
