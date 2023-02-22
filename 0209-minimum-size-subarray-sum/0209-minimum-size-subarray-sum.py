7856011009853d90cf1c5aaad74ee73a1249ed62
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-02-22 21:11:01 +0300

    Time: 288 ms (28.69%), Space: 27.1 MB (84.17%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -2,6 +2,7 @@ class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         left = 0
         right = 0
+        
         minimum = float("inf")
         add = nums[0]
         
@@ -12,7 +13,7 @@ class Solution:
                 add -= nums[left]
                 left += 1 
                 
-            elif add < target:
+            else:
                 right += 1
                 
                 if right < len(nums):
