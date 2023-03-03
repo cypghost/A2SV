b75f6f59a76b0a5ce97cab31151ba09e54fdbc8d
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-03-03 11:24:34 +0300

    Time: 35 ms (41.38%), Space: 13.8 MB (50.70%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,17 +3,19 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        low = 1
-        high = n
-        bad = 0
-        while low <= high:
-            mid = (high + low) // 2
+#         low = 1
+#         high = n
+#         bad = 0
+#         while low <= high:
+#             mid = (high + low) // 2
             
-            if isBadVersion(mid):
-                bad = mid
-                high = mid - 1 
+#             if isBadVersion(mid):
+#                 bad = mid
+#                 high = mid - 1 
             
-            else:
-                low = mid + 1
+#             else:
+#                 low = mid + 1
                 
-        return bad
+#         return bad
+
+        return bisect_left(range(n), True, 1, key=isBadVersion)
