9cca16d047983e7d4928d37f9a8f7f21eebc118f
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-03-05 15:16:07 +0300

    Time: 1094 ms (95.07%), Space: 29.8 MB (96.06%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -1,8 +1,12 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        top, bottom, res = sum(grid[0]), 0, math.inf
-        for g0, g1 in zip(grid[0], grid[1]):
-            top -= g0
+        top = sum(grid[0])
+        bottom = 0
+        res = float("inf")
+        
+        for idx, val in zip(grid[0], grid[1]):
+            top -= idx
             res = min(res, max(top, bottom))
-            bottom += g1
+            bottom += val
+            
         return res
