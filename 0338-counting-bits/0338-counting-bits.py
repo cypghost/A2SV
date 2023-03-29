30187c571adb87fcfcdeb56e0a1b87ada8ff219f
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-03-29 18:12:57 +0300

    Time: 398 ms (5.02%), Space: 111.8 MB (19.07%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        
+        def check(start):
+            temp = start
+            
+            if temp >= len(ans):
+                return ans
+            
+            count = 0
+            
+            if start == 1:
+                ans[start] += 1
+                
+            else:
+                res = ""
+                while start > 0:
+                    rem = start % 2
+                    res += str(rem)
+                    start = start // 2
+                    
+                ans[temp] += res.count("1")
+                
+            check(temp + 1)
+            
+        check(0)
+        
+        return ans
+        
+        
+                
+            
