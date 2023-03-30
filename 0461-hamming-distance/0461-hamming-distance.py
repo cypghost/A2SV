2fdcd2b45e4b38d1f1738d1598fd37292bdcb130
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-03-30 18:54:06 +0300

    Time: 37 ms (21.86%), Space: 13.7 MB (99.85%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.py
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -1,3 +1,10 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        return (x ^ y).bit_count()
+        ans = 0
+        
+        for index in range(max(x, y).bit_length()):
+            ans += (x & 1 << index) != (y & 1 << index)
+        
+        return ans
+    
+        # return (x ^ y).bit_count()
