27527eaf9fda0ca2dc013073c5cacfaf80deb378
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-04-05 04:59:06 +0300

    Time: 334 ms (62.00%), Space: 25.7 MB (6.08%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.py
@@ -0,0 +1,53 @@
+class Solution:
+    def distinctPrimeFactors(self, nums: List[int]) -> int:
+#         def primefactorization(prod):    
+#             prime = 2
+#             ans = set()
+            
+#             while prime * prime <= prod:
+#                 while prod % prime ==  0:
+#                     ans.add(prime)
+#                     prod //= prime
+                    
+#                 prime += 1        
+  	  
+#             if prod > 1:
+#                 ans.add(prod)
+                
+#             return len(ans)
+        
+#         prod = 1
+        
+#         for num in nums:
+#             prod *= num
+        
+#         return primefactorization(prod)
+
+        ans = set()
+    
+        def primefactorization(num, ans):
+            if num == len(nums):
+                return len(ans)
+            
+            prime = 2
+        
+            while prime * prime <= nums[num]:
+                while nums[num] % prime == 0:
+                    ans.add(prime)
+                    nums[num] //= prime
+                
+                prime += 1
+                
+            if nums[num] > 1:
+                ans.add(nums[num])
+            
+            primefactorization(num + 1, ans)
+            
+            return len(ans)
+        
+        return primefactorization(0, ans)
+                
+            
+
+
+        
