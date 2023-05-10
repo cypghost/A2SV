ba4b3896b0b91f9538d13d0e12953ee71d9768db
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-05-10 18:43:53 +0300

    Time: 642 ms (91.52%), Space: 23.4 MB (26.78%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,37 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        color = [0] * len(graph)
+        order = []
+        
+        def dfs(node):
+            if color[node] == 2:
+                return False
+        
+            if color[node] == 1:
+                return True
+            
+            color[node] = 1
+        
+            for neighbor in graph[node]:
+                cycle_found = dfs(neighbor)
+                
+                if cycle_found:
+                    return True
+            
+            color[node] = 2
+            order.append(node)
+            return False
+        
+        for index in range(len(graph)):
+            cycle_found = dfs(index)
+            
+            if cycle_found:
+                continue
+        
+        # print(color)
+        # print(order)
+        order.sort()
+        return order
+        
+        
+                
