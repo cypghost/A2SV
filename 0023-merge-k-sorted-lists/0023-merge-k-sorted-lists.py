8a6c31b8231d6c3884a7a173a37ac873825494ea
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-05-14 20:05:04 +0300

    Update 0023-merge-k-sorted-lists.py

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,6 +3,40 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# HEAP
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        n = len(lists)
+        
+        min_heap = []
+        
+        heapq.heapify(min_heap)
+        
+        for index in range(n):
+            if lists[index]:
+                temp_val = lists[index].val
+                lists[index] = lists[index].next
+                heapq.heappush(min_heap,[temp_val, index])
+            
+        head = ListNode()
+        node = head
+        
+        while min_heap:
+            val, ind = heapq.heappop(min_heap)
+            node_i = ListNode(val = val)
+            node.next = node_i
+            node = node.next
+            
+            if lists[ind]:
+                temp_val = lists[ind].val
+                lists[ind] = lists[ind].next
+                heapq.heappush(min_heap, [temp_val,ind])
+          
+        return head.next
+
+# LINKED LISTS
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         v = []
