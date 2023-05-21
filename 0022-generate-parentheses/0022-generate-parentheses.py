ef8cedf591f29c37c6826cbbb1d7158e0c3ca83e
BIRUK M <102286732+cypghost@users.noreply.github.com>
2023-05-21 16:17:28 +0300

    Time: 52 ms (23.26%), Space: 16.5 MB (39.40%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,33 +1,23 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def isValid(p_string):
-            left_count = 0
-            
-            for p in p_string:
-                if p == '(':
-                    left_count += 1
-                    
-                else:
-                    left_count -= 1
-
-                if left_count < 0:
-                    return False
-                
-            return left_count == 0
-        
         answer = []
-        queue = collections.deque([""])
         
-        while queue:
-            cur_string = queue.popleft()
-
+        def backtracking(cur_string, left_count, right_count):
             if len(cur_string) == 2 * n:
-                if isValid(cur_string):
-                    answer.append(cur_string)
-                    
-                continue
+                answer.append("".join(cur_string))
                 
-            queue.append(cur_string + ")")
-            queue.append(cur_string + "(")
+                return
             
+            if left_count < n:
+                cur_string.append("(")
+                backtracking(cur_string, left_count + 1, right_count)
+                cur_string.pop()
+                
+            if right_count < left_count:
+                cur_string.append(")")
+                backtracking(cur_string, left_count, right_count + 1)
+                cur_string.pop()
+                
+        backtracking([], 0, 0)
+        
         return answer
