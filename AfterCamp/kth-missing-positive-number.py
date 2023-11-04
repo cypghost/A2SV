11273b5cb390892f20dbada4326b4880a2c83416
Biruk Mesfin <102286732+cypghost@users.noreply.github.com>
2023-11-04 14:43:26 +0300

    Added solution for Kth Missing Positive Number

---
+++ AfterCamp/kth-missing-positive-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        for index in range(1, 2001):
+            if index not in arr:
+                 k -= 1
+            
+            if k == 0:
+                return index
+                break
+        
+        return 0
+        
+                    
