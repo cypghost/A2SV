4a86bb52536dc2cd405cefe5cb7e6a6e42705665
Biruk Mesfin <102286732+cypghost@users.noreply.github.com>
2023-12-12 14:19:31 +0300

    Time: 48 ms (53.00%), Space: 16.4 MB (15.70%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,18 +1,18 @@
 class Solution:
-    def twoCitySchedCost(self, costs: List[List[int]]) -> int:  
-        dictionary = defaultdict(int)
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int: 
+        n, difference = len(costs), {}
         sumA, sumB = 0, 0
-
-        for index in range(len(costs)):
-            dictionary[index] = costs[index][0] - costs[index][1]
-
-        dictionary = sorted(dictionary.items(), key=lambda x:x[1])
         
-        for index in range(len(dictionary)):
-            if index < len(dictionary) // 2:
-                sumA += costs[dictionary[index][0]][0]
-
-            if index >= len(dictionary) // 2:
-                sumB += costs[dictionary[index][0]][1]
-
+        for index in range(n):
+            difference[index] = costs[index][0] - costs[index][1]
+        
+        difference = sorted(difference.items(), key=lambda x:x[1])
+        
+        for index in range(n):
+            if index < n // 2:
+                sumA += costs[difference[index][0]][0]
+            
+            else:
+                sumB += costs[difference[index][0]][1]
+        
         return sumA + sumB
