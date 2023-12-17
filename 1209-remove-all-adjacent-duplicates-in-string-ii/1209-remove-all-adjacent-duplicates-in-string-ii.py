359b5cbc4399f5c9e22eec93a2ab51ac5dd15f6a
Biruk Mesfin <102286732+cypghost@users.noreply.github.com>
2023-12-17 23:28:46 +0300

    Time: 96 ms (57.41%), Space: 21.2 MB (14.02%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -1,57 +1,16 @@
-# class TrieNode:
-#     def __init__(self):
-#         self.children = {}
-#         self.isEndofWord = False
-
-# class Trie:
-#     def __init__(self):
-#         self.root = TrieNode()
-    
-#     def insert(self, word: str) -> None:
-#         node = self.root
-
-#         for char in word:
-#             if char not in node.children:
-#                 node.children[char] = TrieNode()
-            
-#             node = node.children[char]
-        
-#         node.isEndofWord = True
-    
-#     def search(self, word: str) -> bool:
-#         node = self.root
-
-#         for char in word:
-#             if char not in node.children:
-#                 return False
-        
-#         return self.isEndofWord
-    
-#     def startsWith(self, word: str) -> bool:
-#         node = self.root
-
-#         for char in word:
-#             if char not in node.children:
-#                 return False
-        
-#         return True
-
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:        
-        stack = [['', 0]]    
-
-        for c in s:
-            if stack[-1][0] == c:
-                stack[-1][1] += 1 
-
+        stack = [["", 1]]
+        
+        for i in s:
+            if stack[-1][0] == i:
+                stack[-1][1] += 1
+                
                 if stack[-1][1] == k:
                     stack.pop()
-
-            else:
-                stack.append([c, 1])            
-        
-        return ''.join(c * count for c, count in stack)
             
+            else:
+                stack.append([i, 1])
         
-
+        return "".join(c * count for c, count in stack)
         
